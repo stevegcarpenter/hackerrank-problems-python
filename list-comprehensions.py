b18c1e6ef298299f2c95a44bc9b4b3484f99c5cc
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-13 10:35:19 -0700

    Solution to list comprehensions

---
+++ list-comprehensions.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env/ python3
+
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+x, y, z, n = (int(input()) for _ in range(4))
+print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
